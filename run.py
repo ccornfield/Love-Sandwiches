9a3b5e74ed17bb5e878b0c9b90da608554d21149
ccornfield <ecolus48@gmail.com>
2024-07-26 16:00:47 +0000

    Completed adding sales data section.

--- run.py
+++ run.py
@@ -29,6 +29,8 @@ def get_sales_data():
         if validate_data(sales_data):
             print("Data is valid!")
             break
+    
+    return sales_data
 
 
 def validate_data(values):
@@ -47,4 +49,16 @@ def validate_data(values):
     
     return True
 
-get_sales_data()
+def update_sales_update(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_update(sales_data)
+
